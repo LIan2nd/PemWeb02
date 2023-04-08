be771b59e825f5768fa8e86fe2ac0d8980ce34c6
LIan2nd <alfiannurusyaid19@gmail.com>
2023-04-08 08:26:47 +0700

    Praktikum dan Tugas P7

--- P7_Alfian Nur Usyaid_TI07/motor.php
+++ P7_Alfian Nur Usyaid_TI07/motor.php
@@ -18,20 +18,24 @@ class Motor
     public function jalan($jarak) //Kilo Meter
     {
         $jalan = $jarak / 30;
+        $waktu = $jarak / $this->topSpeed;
         $this->bensin -= $jalan;
 
-        echo "Motor $this->merk $this->cc CC telah berjalan sepanjang $jarak KM dengan kecepatan  $this->topSpeed km/h dan sisa bensin yang tersisa yaitu $this->bensin Liter <hr><br>";
+        echo "Motor <b>$this->merk $this->cc CC</b> telah berjalan sepanjang <b>$jarak km</b> dengan kecepatan <b>$this->topSpeed km/h</b>, sampai ke tempat tujuan dengan waktu <b>$waktu Jam</b> dan bensin yang tersisa yaitu <b>$this->bensin Liter</b> <hr><br>";
     }
 }
 
 $motor = new Motor('Vario', 3, 500, 250);
 $motor->jalan(30);
 
-$motor2 = new Motor('Supra', 10, 600, 600);
+$motor2 = new Motor('Supra', 10, 600, 650);
 $motor2->jalan(10);
 
-$motor3 = new Motor('NMax', 1, 750, 1000);
+$motor3 = new Motor('NMax', 1, 750, 1500);
 $motor3->jalan(30);
+
+$motor4 = new Motor('PowerRanger', 2, 40, 10000);
+$motor4->jalan(60);
 
 
 ?>
